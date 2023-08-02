81fd487dc746e3ce73bd05b7e02768ef27162014
AlejandroEsteveMaza <alex.esteve98@gmail.com>
2023-08-02 18:14:17 +0200

    Auto-errors and session

--- src/main/java/com/springboot/form/app/controllers/FormController.java
+++ src/main/java/com/springboot/form/app/controllers/FormController.java
@@ -1,45 +1,44 @@
 package com.springboot.form.app.controllers;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
 
 import com.springboot.form.app.models.domain.Usuario;
 
 import jakarta.validation.Valid;
 
 @Controller
+@SessionAttributes("usuario")
 public class FormController {
 
 	@GetMapping("/form")
 	public String form(Model model) {
 		Usuario usuario = new Usuario();
+		usuario.setNombre("John");
+		usuario.setApellido("Doe");
+		usuario.setIdentificador("15612.13k");
 		model.addAttribute("titulo", "Formulario Usuarios");
 		model.addAttribute("usuario", usuario);
-		
+
 		return "form";
-		
+
 	}
 
 	@PostMapping("/form")
-	public String procesar(@Valid Usuario usuario, BindingResult result, Model model) {
+	public String procesar(@Valid Usuario usuario, BindingResult result, Model model, SessionStatus status) {
 		model.addAttribute("titulo", "Resultado Form");
 		if (result.hasErrors()) {
-			Map<String, String> errores = new HashMap<>();
-			result.getFieldErrors().forEach(err->{
-				errores.put(err.getField(), err.getDefaultMessage());
-			});
-			model.addAttribute("error", errores);
+
 			return "form";
 		}
 
 		model.addAttribute("usuario", usuario);
-
+		status.setComplete();
 		return "resultado";
 	}
 
